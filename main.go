47d0c0dc7f444c768d10cc9cee6398b6ca65a143
ธิเบต-เพชรรอด-HQ-MI dohome <7547647-Thibet@users.noreply.gitlab.com>
2021-01-13 17:39:03 +0700

    remove env

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"ginRestFulApi/src/routers"
 	"ginRestFulApi/utils"
 
@@ -12,6 +11,5 @@ func main() {
 	router := routers.InitRoute() // apply api router
 	gin.SetMode(utils.EnvVar("GIN_MODE", "debug"))
 	port := utils.EnvVar("SERVER_PORT", ":8080")
-	fmt.Println("http://locahost" + port)
 	router.Run(port) // listen to given port
 }
